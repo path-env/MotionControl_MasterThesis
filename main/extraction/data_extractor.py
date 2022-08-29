e2cce967d6694152d5e359f316f7bb0213653850
path_env <mankaldeep@gmail.com>
2022-08-29 16:08:27 +0200

    Updates for new models

--- main/extraction/data_extractor.py
+++ main/extraction/data_extractor.py
@@ -1,47 +1,75 @@
-from tabnanny import check
+import sys
+import numpy as np
+from sklearn.model_selection import train_test_split
+sys.path.append('/media/mangaldeep/HDD2/workspace/MotionControl_MasterThesis')
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
+from torch.utils.data.sampler import SubsetRandomSampler
 
 # from mne.datasets import eegbci
-from data_shredder.physionet import extractPhysioNet
+# from data_shredder.physionet import extractPhysioNet
 from data.fake_data import extractFakeData
 
 class data_container(Dataset):
-    def __init__(self, check_net = False) -> None:
+    def __init__(self, feat, label, check_net = False) -> None:
         super().__init__()
         self.channels = 64
         if check_net:
             train, test = extractFakeData()            
-            self.train_x = train[0]
-            self.train_y = train[1]
-            self.feat_shape = self.train_x.shape
+            self.x = train[0]
+            self.y = train[1]
 
             self.test_x = test[0]
             self.test_y = test[1]
         else:
-            feat, label = extractPhysioNet(16,8)
-            self.__test_train_split__(feat, label)
+            self.x = feat
+            self.y = label
+            
+            # feat, label = extractPhysioNet(16,8)
+            # self.__test_train_split__(feat, label)
     
     def __getitem__(self, index):
-        return self.x[index,:]
+        return self.x[index,:], self.y[index]
     
     def __len__(self):
-        return self.len
+        return len(self.y)
     
     def __test_train_split__(self, feat, label):
-        self.feat_shape = feat.shape
-        self.train_x = torch.tensor(feat)
-        self.train_y = torch.tensor(feat)
+        # Train - Test split
+        self.train_x, self.test_x,self.train_y,self.test_y =train_test_split(feat, label, test_size= 0.25, stratify= label,random_state= 42)
+        # print(random_split(feat,[3, 7], generator=torch.Generator().manual_seed(42)))
+        # self.feat_shape = feat.shape
+        # self.train_x = torch.tensor(feat)
+        # self.train_y = torch.tensor(feat)
 
-        self.test_x = torch.tensor(feat)
-        self.test_y = torch.tensor(feat)      
+        # self.test_x = torch.tensor(feat)
+        # self.test_y = torch.tensor(feat)      
     
-    # def __getattribute__(self, index):
-    #     return self.y[index]
-
 if __name__ == "__main__":
     # dd= eegbci.load_data(1, [4, 10, 14], "/media/mangaldeep/HDD3/DataSets/MNEPhysioNet")
-    data = data_container(check_net = True)
-    trainloader = DataLoader(dataset=data, batch_size=1)
+    feat = np.random.randn(30,64,227)
+    labels = np.concatenate([-np.ones(15), np.ones(15)])
+    data = data_container(feat, labels, check_net = False)
+    validation_split = 0.25
+    shuffle_dataset = True
+    random_seed = 42
+    batch_size = 5
+    # Input data & labels
+    dataset_size = len(data)
+    indices = list(range(dataset_size))
+    split = int(np.floor(validation_split * dataset_size))
+    if shuffle_dataset :
+        np.random.seed(random_seed)
+        np.random.shuffle(indices)
+    train_indices, val_indices = indices[split:], indices[:split]
+
+    # Creating PT data samplers and loaders:
+    train_sampler = SubsetRandomSampler(train_indices)
+    valid_sampler = SubsetRandomSampler(val_indices)
+
+    train_loader = torch.utils.data.DataLoader(data, batch_size=batch_size, 
+                                            sampler=train_sampler)
+    validation_loader = torch.utils.data.DataLoader(data, batch_size=batch_size,
+                                                    sampler=valid_sampler)
     # dsets.MNIST(root='./data', download=True, transform=transforms.ToTensor())
 
--- models/train_net.py
+++ models/train_net.py
@@ -1,43 +1,183 @@
+import sys
+sys.path.append('/media/mangaldeep/HDD2/workspace/MotionControl_MasterThesis')
 # from torch import nn, optim
+import time
 import torch
 from torch.utils.tensorboard import SummaryWriter
 from sklearn.model_selection import StratifiedKFold
 
-from data_extractor import data_container
+from extraction.data_extractor import data_container
 
 #%% May have to change this!!!!
 from models.DeepRNN import DRNN
 from models.neurotec_edu import Neurotech_net
 from data.params import NeuroTechNetParams
-model = Neurotech_net()
-params = NeuroTechNetParams()
-# Loss Function
-criteria = torch.nn.MSELoss()
-# Optimizer
-optimizer = torch.optim.Adam(model.parameters(), lr=params.learning_rate)
-# Tensorboard writer
-writer = SummaryWriter('runs/fake_data')
 
-#%% Model Training
-# create batches
-dataset = data_container(check_net = True)
-writer.add_graph(model, dataset.train_x)
-for n_iter,epoch in enumerate(range(1000)):
-    # for x,y in zip(dataset.train_x, dataset.train_y):
-    optimizer.zero_grad()
-    yhat = model(dataset.train_x)
-    loss = criteria(yhat, dataset.train_y)
-    loss.backward()
-    optimizer.step()
-    writer.add_scalar('Loss/train', loss, n_iter)
-    # writer.add_scalar('Loss/test', np.random.random(), n_iter)
-    # writer.add_scalar('Accuracy/train', np.random.random(), n_iter)
-    # writer.add_scalar('Accuracy/test', np.random.random(), n_iter)
-# writer.add_scalar()
-writer.close()
+def train_and_validate(model,train_loader, validation_loader, loss_criterion, optimizer, LRscheduler, epochs=25):
+    '''
+    Function to train and validate
+    Parameters
+        :param model: Model to train and validate
+        :param loss_criterion: Loss Criterion to minimize
+        :param optimizer: Optimizer for computing gradients
+        :param epochs: Number of epochs (default=25)
+  
+    Returns
+        model: Trained Model with best validation accuracy
+        history: (dict object): Having training loss, accuracy and validation loss, accuracy
+    '''
+    
+    start = time.time()
+    writer = SummaryWriter('runs/fake_data')
+    history = []
 
-# torch.save(model, "/media/mangaldeep/HDD2/workspace/MotionControl_MasterThesis/models/model_check")
-#%% Model Testing
-# import torch
-# model = torch.load("/media/mangaldeep/HDD2/workspace/MotionControl_MasterThesis/models/model_check")
-test_results = model(dataset.test_x).data.tolist()
+    best_acc = 0.0
+    # device = 'cuda' if torch.cuda.is_available() else "cpu"
+    device = 'cpu'
+    # model = model.cuda() if torch.cuda.is_available() else model
+
+    # writer.add_graph(model, train_loader.x)
+    for epoch in range(epochs):
+        train_data_size = 0 #train_loader.dataset.x.shape[0]
+        validation_data_size = 0 #validation_loader.dataset.x.shape[0]
+        epoch_start = time.time()
+        print("Epoch: {}/{}".format(epoch+1, epochs))
+        
+        # Set to training mode
+        model.train()
+        
+        # Loss and Accuracy within the epoch
+        train_loss = 0.0
+        train_acc = 0.0
+        
+        valid_loss = 0.0
+        valid_acc = 0.0
+        
+        for i, (inputs, labels) in enumerate(train_loader):
+
+            inputs = inputs.to(device)
+            labels = labels.to(device).float()
+            
+            # Clean existing gradients
+            optimizer.zero_grad()
+            
+            # Forward pass - compute outputs on input data using the model
+            outputs = model(inputs).squeeze(0)
+            labels = labels.view_as(outputs)
+            
+            # Compute loss
+            loss = loss_criterion(outputs, labels)
+            
+            # Backpropagate the gradients
+            loss.backward()
+            
+            # Update the parameters
+            optimizer.step()
+            
+            # Compute the total loss for the batch and add it to train_loss
+            train_loss += loss.item() * inputs.size(0)
+            
+            # Compute the accuracy
+            ret, predictions = torch.max(outputs.data, -1)
+            correct_counts = predictions.eq(labels.data.view_as(predictions))
+            
+            # Convert correct_counts to float and then compute the mean
+            acc = torch.mean(correct_counts.type(torch.FloatTensor))
+            
+            # Compute total accuracy in the whole batch and add to train_acc
+            train_acc += acc.item() * inputs.size(0)
+            
+            train_data_size+= train_loader.batch_size
+            # print("Batch number: {:03d}, Training: Loss: {:.4f}, Accuracy: {:.4f}".format(i, loss.item(), acc.item()))
+
+        LRscheduler.step()    
+
+        # Validation - No gradient tracking needed
+        with torch.no_grad():
+
+            # Set to evaluation mode
+            model.eval()
+
+            # Validation loop
+            for j, (inputs, labels) in enumerate(validation_loader):
+                inputs = inputs.to(device)
+                labels = labels.to(device).float()
+
+                # Forward pass - compute outputs on input data using the model
+                outputs = model(inputs).squeeze(0)
+                labels = labels.view_as(outputs)
+
+                # Compute loss
+                loss = loss_criterion(outputs, labels)
+
+                # Compute the total loss for the batch and add it to valid_loss
+                valid_loss += loss.item() * inputs.size(0)
+
+                # Calculate validation accuracy
+                ret, predictions = torch.max(outputs.data, -1)
+                correct_counts = predictions.eq(labels.data.view_as(predictions))
+
+                # Convert correct_counts to float and then compute the mean
+                acc = torch.mean(correct_counts.type(torch.FloatTensor))
+
+                # Compute total accuracy in the whole batch and add to valid_acc
+                valid_acc += acc.item() * inputs.size(0)
+
+                validation_data_size+= validation_loader.batch_size
+                #print("Validation Batch number: {:03d}, Validation: Loss: {:.4f}, Accuracy: {:.4f}".format(j, loss.item(), acc.item()))
+            
+        # Find average training loss and training accuracy
+        avg_train_loss = train_loss/train_data_size 
+        avg_train_acc = train_acc/train_data_size
+
+        # Find average training loss and training accuracy
+        avg_valid_loss = valid_loss/validation_data_size 
+        avg_valid_acc = valid_acc/validation_data_size
+
+        history.append([avg_train_loss, avg_valid_loss, avg_train_acc, avg_valid_acc])
+                
+        epoch_end = time.time()
+        
+        # print(train_data_size)
+        # print(validation_data_size)
+        print("Epoch : {:03d}, Training: Loss: {:.4f}, Accuracy: {:.4f}%, \n\t\tValidation : Loss : {:.4f}, Accuracy: {:.4f}%, Time: {:.4f}s".format(epoch+1, avg_train_loss, avg_train_acc*100, avg_valid_loss, avg_valid_acc*100, epoch_end-epoch_start))
+        
+        # Save if the model has best accuracy till now
+        #torch.save(model, dataset+'_model_'+str(epoch)+'.pt')
+    writer.close()       
+    return model, history
+
+#%%
+if __name__ == "__main__":
+    model = Neurotech_net()
+    params = NeuroTechNetParams()
+    # Loss Function
+    criteria = torch.nn.MSELoss()
+    # Optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=params.learning_rate)
+    # Tensorboard writer
+    writer = SummaryWriter('runs/fake_data')
+
+    #%% Model Training
+    # create batches
+    dataset = data_container(check_net = True)
+    writer.add_graph(model, dataset.train_x)
+    for n_iter,epoch in enumerate(range(1000)):
+        # for x,y in zip(dataset.train_x, dataset.train_y):
+        optimizer.zero_grad()
+        yhat = model(dataset.train_x)
+        loss = criteria(yhat, dataset.train_y)
+        loss.backward()
+        optimizer.step()
+        writer.add_scalar('Loss/train', loss, n_iter)
+        # writer.add_scalar('Loss/test', np.random.random(), n_iter)
+        # writer.add_scalar('Accuracy/train', np.random.random(), n_iter)
+        # writer.add_scalar('Accuracy/test', np.random.random(), n_iter)
+    # writer.add_scalar()
+    writer.close()
+
+    # torch.save(model, "/media/mangaldeep/HDD2/workspace/MotionControl_MasterThesis/models/model_check")
+    #%% Model Testing
+    # import torch
+    # model = torch.load("/media/mangaldeep/HDD2/workspace/MotionControl_MasterThesis/models/model_check")
+    test_results = model(dataset.test_x).data.tolist()
